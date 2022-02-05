c46e1ed402ae57aa820c60267fca108e046a8932
Rohan Saroha <rohansaroha@Rohans-MacBook-Pro.local>
2022-02-06 03:37:40 +0530

    added rtk query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import {store} from '../store/index'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+  </>
+  )
 }
 
 export default MyApp
