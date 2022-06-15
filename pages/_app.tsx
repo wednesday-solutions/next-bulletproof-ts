35a5d48ce28b03966251a88c962e6183f7d171b4
mohit-wednesday <mohit.kushwaha@wednesday.is>
2022-06-15 15:20:30 +0530

    feat: add viewport meta to custom app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,25 @@ import { store } from "@store";
 import { IntlProvider } from "react-intl";
 import messages from "../translations/en.json";
 import { ErrorBoundary } from "@common";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <IntlProvider messages={messages} locale="en" defaultLocale="en">
-      <Provider store={store}>
-        <ErrorBoundary>
-          <Component {...pageProps} />
-        </ErrorBoundary>
-      </Provider>
-    </IntlProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <IntlProvider messages={messages} locale="en" defaultLocale="en">
+        <Provider store={store}>
+          <ErrorBoundary>
+            <Component {...pageProps} />
+          </ErrorBoundary>
+        </Provider>
+      </IntlProvider>
+    </>
   );
 };
 
