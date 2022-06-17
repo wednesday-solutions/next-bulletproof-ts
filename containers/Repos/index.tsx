59a70555c3009eaaf270aa22245e350fa638dfb5
mohit-wednesday <mohit.kushwaha@wednesday.is>
2022-06-17 16:08:29 +0530

    feat: add pagination and search param using shallow routing

--- containers/Repos/index.tsx
+++ containers/Repos/index.tsx
@@ -1,13 +1,14 @@
-import { Container, CustomCard, T } from "@common";
+import { Container, CustomCard, If, T } from "@common";
 import { useFetchRecommendationQuery } from "@features/repos/api/getRecommendations";
 import { ErrorState, Recommended, RepoList, YouAreAwesome } from "@features/repos/components";
 import { IRepoError, Recommendation } from "@features/repos/types";
 import { fonts } from "@themes/index";
-import { Divider, Input, Row } from "antd";
-import { debounce } from "lodash-es";
-import React, { memo, useState } from "react";
+import { Divider, Input, Row, Button } from "antd";
+import { debounce, get, isEmpty } from "lodash-es";
+import React, { memo, useEffect, useState } from "react";
 import { injectIntl, IntlShape } from "react-intl";
 import { compose } from "redux";
+import { useRouter } from "next/router";
 
 const { Search } = Input;
 
@@ -19,11 +20,43 @@ interface RepoContainerProps {
 }
 
 export const Repos: React.FC<RepoContainerProps> = ({ intl, maxwidth, recommendations }) => {
+  const router = useRouter();
   const [repoName, setRepoName] = useState<string>("");
-  const { data, error, isLoading, isFetching } = useFetchRecommendationQuery(repoName);
+  const [page, setPage] = useState<number>(1);
+  const nextPage = () => {
+    setPage(val => val + 1);
+    router.push(`/?search=${repoName}&page=${+page + 1}`, undefined, {
+      shallow: true,
+    });
+  };
+  const prevPage = () => {
+    setPage(val => val - 1);
+    router.push(`/?search=${repoName}&page=${page - 1}`, undefined, {
+      shallow: true,
+    });
+  };
+
+  const { data, error, isLoading, isFetching } = useFetchRecommendationQuery({ repoName, page });
+
+  const isNextButtonDisable: boolean =
+    get(data, "totalCount", 0) <= 10 || get(data, "totalCount", 0) / 10 === page;
+
   const handleOnChange = debounce((rName: string) => {
     setRepoName(rName);
   }, 200);
+
+  useEffect(() => {
+    if (get(router, "isReady", false)) {
+      setRepoName(get(router, "query.search", ""));
+      setPage(get(router, "query.page", 1));
+    }
+  }, [get(router, "isReady")]);
+
+  useEffect(() => {
+    if (!isEmpty(repoName)) {
+      router.push(`/?search=${repoName}&page=${page}`);
+    }
+  }, [data]);
 
   return (
     <Container
@@ -48,13 +81,25 @@ export const Repos: React.FC<RepoContainerProps> = ({ intl, maxwidth, recommenda
         <T marginBottom={10} id="get_repo_details" />
         <Search
           data-testid="search-bar"
-          defaultValue={"test"}
           type="text"
+          value={repoName}
           onChange={evt => handleOnChange(evt.target.value)}
           onSearch={searchText => handleOnChange(searchText)}
         />
       </CustomCard>
       <RepoList reposData={data} repoName={"test"} loading={isLoading && isFetching} />
+      <If condition={data}>
+        <Container padding={20} maxwidth={500}>
+          <Row justify="center">
+            <Button disabled={page === 1} loading={isFetching} onClick={prevPage}>
+              prev
+            </Button>
+            <Button loading={isFetching} onClick={nextPage} disabled={isNextButtonDisable}>
+              next
+            </Button>
+          </Row>
+        </Container>
+      </If>
       <ErrorState
         reposData={data}
         loading={isLoading && isFetching}
