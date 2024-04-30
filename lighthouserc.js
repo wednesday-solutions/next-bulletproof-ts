bbf092dc2e42db01c0fa9a9db6e263749f4b4727
apurv-wednesday <apurv@wednesday.is>
2024-04-30 12:21:56 +0530

    feat: add lighthouse conf

---
+++ lighthouserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "yarn start",
+      url: ["http://localhost:3000"],
+      numberOfRuns: 3,
+      // default value is mobile
+      // settings: {
+      //   preset: "desktop",
+      // },
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
