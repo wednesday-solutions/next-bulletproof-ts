97c049db169c6af7d3b218e81bfc4946a7ef77d8
apurv-wednesday <apurv@wednesday.is>
2024-04-30 14:40:07 +0530

    revert: loosen the lighthouse checks

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,12 +10,13 @@ module.exports = {
       // },
     },
     assert: {
+      // Using loose settings for now. Should be tightened for production builds.
       assertions: {
-        'first-contentful-paint': ['error', { maxNumericValue: 2500 }],
-        'categories:performance': ['error', { minScore: 0.95 }],
-        'categories:accessibility': ['error', { minScore: 0.95 }],
-        'categories:best-practices': ['error', { minScore: 0.95 }],
-        'categories:seo': ['error', { minScore: 0.95 }]
+        'first-contentful-paint': ['error', { maxNumericValue: 4000 }],
+        'categories:performance': ['error', { minScore: 0.90 }],
+        'categories:accessibility': ['error', { minScore: 0.90 }],
+        'categories:best-practices': ['error', { minScore: 0.90 }],
+        'categories:seo': ['error', { minScore: 0.90 }]
       }
     },
     upload: {
