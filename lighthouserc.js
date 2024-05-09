29837970cab69332b138e10c3c532e8fc116bec4
apurv-wednesday <apurv@wednesday.is>
2024-05-09 18:16:11 +0530

    fix: remove FCP assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,14 +4,9 @@ module.exports = {
       startServerCommand: "yarn start",
       url: ["http://localhost:3000"],
       numberOfRuns: 3,
-      // default value is mobile
-      // settings: {
-      //   preset: "desktop",
-      // },
     },
     assert: {
       assertions: {
-        'first-contentful-paint': ['error', { maxNumericValue: 2500 }],
         'categories:performance': ['error', { minScore: 0.90 }],
         'categories:accessibility': ['error', { minScore: 0.90 }],
         'categories:best-practices': ['error', { minScore: 0.90 }],
