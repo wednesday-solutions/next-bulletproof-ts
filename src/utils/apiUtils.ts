bf9f80be9c0317055e27e7cdf1bd0ce8638f80e9
Anurag-Wednesday <anurag@wednesday.is>
2024-09-10 10:58:03 +0530

    fix: fixed issues with test and removed axios

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,42 +1,32 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import { get } from "lodash";
-import axios, { AxiosError } from "axios";
+import { RootState } from "@app/store";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import isomorphicFetch from "isomorphic-fetch";
 import { HYDRATE } from "next-redux-wrapper";
 import { Action, PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "@app/store";
 
 function isHydrateAction(action: Action): action is PayloadAction<RootState> {
   return action.type === HYDRATE;
 }
-
-const axiosBaseQuery =
-  ({ baseUrl } = { baseUrl: "" }) =>
-  async ({ url, method, data, params }) => {
-    try {
-      const result = await axios({ url: baseUrl + url, method, data, params });
-      return { data: result.data };
-    } catch (err) {
-      return {
-        error: {
-          status: (err as AxiosError).response?.status,
-          data: (err as AxiosError).response?.data || (err as Error).message,
-        },
-      };
-    }
-  };
 
 /**
  * @desc Here we initialize an empty api service that we'll inject endpoints into later as needed
  */
 export const githubApiService = createApi({
   reducerPath: "github",
-  baseQuery: axiosBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_GITHUB_URL || "",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_GITHUB_URL,
+    fetchFn: isomorphicFetch,
   }),
+  // to fix the error: "Type alias 'RootState' circularly references itself". return type is set to any
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   extractRehydrationInfo(action, { reducerPath }): any {
-    if (isHydrateAction(action)) {
-      return get(action.payload, reducerPath);
+    if (
+      isHydrateAction(action) &&
+      action.payload &&
+      typeof action.payload === "object" &&
+      reducerPath in action.payload
+    ) {
+      return action.payload[reducerPath as keyof typeof action.payload];
     }
   },
   endpoints: () => ({}),
